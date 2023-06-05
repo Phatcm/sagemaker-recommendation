f9be53cf87fd296256fd8a2d5e9dbb79f9316ce5
phatcm <phat.caominh.dev@gmail.com>
2023-06-05 17:16:45 +0700

    kaggle api

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import kaggle
 
-# Download the dataset
-kaggle.api.competition_download_files('titanic')
+# Specify the dataset URL
+dataset_url = 'https://www.kaggle.com/datasets/thedevastator/grocery-product-prices-for-australian-states?fbclid=IwAR1KnP3kiv2h1NrufSHApmnr6IUGjTpDg5UQ0KNXBpb6A7Apu3hT5WXk2HM'
 
-# Specify the destination directory (optional)
-kaggle.api.competition_download_files('titanic', path='/path/to/save')
+# Download the dataset
+kaggle.api.competition_download_files(dataset_url, path='/resources')
 
 # Extract the contents of the ZIP file (optional)
 import zipfile
-with zipfile.ZipFile('/path/to/save/titanic.zip', 'r') as zip_ref:
-    zip_ref.extractall('/path/to/extract')
+with zipfile.ZipFile('/resources/titanic.zip', 'r') as zip_ref:
+    zip_ref.extractall('/resources/')
